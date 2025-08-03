354b0bbf4d660b376254c2b7e35bd089eb0971cb
rachelann1028 <rachelanncastaneda@gmail.com>
2025-08-03 17:24:05 -0600

    Fix: bind Flask to 0.0.0.0 with PORT env var

--- flask_api_backend.py
+++ flask_api_backend.py
@@ -637,4 +637,7 @@ if __name__ == '__main__':
     print(f"\n Server running at: http://127.0.0.1:5000")
     print(" Chart data generation is now FIXED and working!")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
