7880273886d192d07f99a467270b8a0c7620a923
rachelann1028 <rachelanncastaneda@gmail.com>
2025-08-03 17:44:06 -0600

    Bind to 0.0.0.0 and use dynamic PORT for Render

--- flask_api_backend.py
+++ flask_api_backend.py
@@ -641,3 +641,7 @@ import os
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+from flask import Flask
+
+app = Flask(__name__)
